66a8ecbe9e7a1e0f4ed0863d87c5f3bf254e7d50
Isedin Karamehic <isedin_karamehic@teilnehmer.cdemy.de>
2024-09-24 16:57:20 +0200

    the logic for login and new user register added

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\UserDetails;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * login.
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        //create a controller to handle incoming requests and return some data
+        //validate incoming inputs
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        //check if the user exists in the database
+        $user = User::where('email', $request->email)->first();
+
+        //check password
+        if (!$user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['These credentials do not match our records.'],
+            ]);
+        }
+
+        //then return generated token
+        return $user->createToken($request->email)->plainTextToken;
+    }
+
+    /**
+     * register.
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming inputs
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'type' => 'user',
+            'password' => Hash::make($request->password),
+        ]);
+
+        $userInfo = UserDetails::create([
+            'user_id' => $user->id,
+            'status' => 'active',
+        ]);
+
+        return $user;
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [UsersController::class, 'login']);
+Route::post('/register', [UsersController::class, 'register']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+// it could have been written in this form:
+// Route::middleware('auth:sanctum')->get('user', function (Request $request) {
+//     return $request->user();
+// });
