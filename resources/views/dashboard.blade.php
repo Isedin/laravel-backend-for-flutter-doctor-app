88e8e9d042c25a9cb5fc964816f9aa05bac23e61
Isedin Karamehic <isedin_karamehic@teilnehmer.cdemy.de>
2024-10-08 09:52:08 +0200

    doctor dashboard has been done. In same branche i have made a Reviews Database

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,69 @@
         </h2>
     </x-slot>
 
+    <!-- Testing Dashboard -->
+    <!-- {{ $doctor }}
+    {{ $appointments }} -->
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            {{-- build dashboard containers --}}
+            <div class="flex flex-wrap justify-center bg-gray-100 py-10 space-x-4 space-y-4 lg:space-y-0">
+
+                <!-- First Stats Container -->
+                <div class="w-full sm:w-1/2 lg:w-1/4 px-4">
+                    <div class="w-full bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+                        <div class="h-20 bg-blue-500 flex items-center justify-between">
+                            <p class="mr-0 text-white text-lg pl-5">UPCOMING APPOINTMENTS</p>
+                        </div>
+                        <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
+                            <p>TOTAL</p>
+                        </div>
+                        <!-- returns the count of appointments -->
+                        <p class="py-4 text-3xl ml-5">{{ count($appointments) }}</p>
+                    </div>
+                </div>
+
+                <!-- Second Stats Container -->
+                <div class="container mx-auto pr-4">
+                    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+                        <div class="h-20 bg-blue-500 flex items-center justify-between">
+                            <p class="mr-0 text-white text-lg pl-5">PATIENTS</p>
+                        </div>
+                        <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
+                            <p>TOTAL</p>
+                        </div>
+                        <p class="py-4 text-3xl ml-5">{{ $doctor->doctor['patients'] ?? 0}}</p>
+                    </div>
+                </div>
+
+                <!-- Third Stats Container -->
+                <div class="container mx-auto pr-4">
+                    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+                        <div class="h-20 bg-blue-500 flex items-center justify-between">
+                            <p class="mr-0 text-white text-lg pl-5">RATINGS</p>
+                        </div>
+                        <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
+                            <p>TOTAL</p>
+                        </div>
+                        <p class="py-4 text-3xl ml-5">20</p>
+                    </div>
+                </div>
+
+                <!-- Forth Stats Container -->
+                <div class="container mx-auto pr-4">
+                    <div class="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
+                        <div class="h-20 bg-blue-500 flex items-center justify-between">
+                            <p class="mr-0 text-white text-lg pl-5">REVIEWS</p>
+                        </div>
+                        <div class="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
+                            <p>TOTAL</p>
+                        </div>
+                        <p class="py-4 text-3xl ml-5">20</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
