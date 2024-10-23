f454b741a8f03ef4a7d9028cc7a4e2147316702f
Isedin Karamehic <isedin_karamehic@teilnehmer.cdemy.de>
2024-10-23 15:05:42 +0200

    x

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ Route::post('/register', [UsersController::class, 'register']);
 //this group mean return user's data if authenticated successfully
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UsersController::class, 'index']);
+    Route::post('fav', [UsersController::class, 'storeFavDoc']);
     Route::post('/book', [AppointmentsController::class, 'store']);
+    Route::post('/logout', [AppointmentsController::class, 'logout']);
     Route::post('/reviews', [DocsController::class, 'store']);
     Route::get('/appointments', [AppointmentsController::class, 'index']); // retrieve all appointments of the authenticated user
 });
